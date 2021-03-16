07715bdbc157b16df5e32cc9c8eda76e90e6f100
chris <chris.mahfouz@hotmail.com>
2021-03-16 14:48:39 +0200

    Completed assignment javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activityM="go to the office"){
+
+    return `This Monday, I will ${activityM}.`
+}
+function wrapAdjective(adjective="*"){
+    return function(kind="special"){
+        return  `You are ${adjective}${kind}${adjective}!`
+    }
+}
